d3114bac6f9ced529d62807330254395abd2c382
rchudasa <rchudasa@cern.ch>
2019-03-25 14:49:25 +0100

    flat pt files

---
+++ flat_pt_electron/gen_sim/SingleGammaFlatPt_pythia8_cfi_GEN_SIM.py
@@ -0,0 +1,127 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.19 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# with command line options: SingleGammaFlatPt_pythia8_cfi --mc --eventcontent RAWSIM --datatier GEN-SIM --conditions 103X_upgrade2018_realistic_HI_v9 --step GEN,SIM --geometry DB:Extended --era Run2_2018 --no_exec --customise Configuration/Generator/RandomSeed_cfi.customizeRandomSeed, Configuration/DataProcessing/Utils.addMonitoring
+import FWCore.ParameterSet.Config as cms
+import FWCore.ParameterSet.VarParsing as VarParsing
+
+from Configuration.StandardSequences.Eras import eras
+process = cms.Process('SIM',eras.Run2_2018)
+options = VarParsing.VarParsing('python')
+
+options.outputFile = 'flatpt_gamma_gen_sim.root'
+options.parseArguments()
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.GeometrySimDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('Configuration.StandardSequences.Generator_cff')
+process.load('IOMC.EventVertexGenerators.VtxSmearedRealistic50ns13TeVCollision_cfi')
+process.load('GeneratorInterface.Core.genFilterSummary_cff')
+process.load('Configuration.StandardSequences.SimIdeal_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(10000)
+    #input = cms.untracked.int32(10)
+)
+
+# Input source
+process.source = cms.Source("EmptySource")
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('SingleGammaFlatPt_pythia8_cfi nevts:1'),
+    name = cms.untracked.string('Applications'),
+    version = cms.untracked.string('$Revision: 1.19 $')
+)
+
+# Output definition
+
+process.RAWSIMoutput = cms.OutputModule("PoolOutputModule",
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('generation_step')
+    ),
+    compressionAlgorithm = cms.untracked.string('LZMA'),
+    compressionLevel = cms.untracked.int32(1),
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('GEN-SIM'),
+        filterName = cms.untracked.string('')
+    ),
+    eventAutoFlushCompressedSize = cms.untracked.int32(20971520),
+    fileName = cms.untracked.string(options.outputFile),
+    outputCommands = process.RAWSIMEventContent.outputCommands,
+    splitLevel = cms.untracked.int32(0)
+)
+
+# Additional output definition
+
+# Other statements
+process.XMLFromDBSource.label = cms.string("Extended")
+process.genstepfilter.triggerConditions=cms.vstring("generation_step")
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, '103X_upgrade2018_realistic_HI_v9', '')
+
+process.generator = cms.EDFilter("Pythia8PtGun",
+    PGunParameters = cms.PSet(
+        AddAntiParticle = cms.bool(True),
+        MaxEta = cms.double(2.5),
+        MaxPhi = cms.double(3.14159265359),
+        MaxPt = cms.double(20.0),
+        MinEta = cms.double(-2.5),
+        MinPhi = cms.double(-3.14159265359),
+        MinPt = cms.double(1.0),
+        ParticleID = cms.vint32(11)
+    ),
+    PythiaParameters = cms.PSet(
+        parameterSets = cms.vstring()
+    ),
+    Verbosity = cms.untracked.int32(0),
+    firstRun = cms.untracked.uint32(1),
+    psethack = cms.string('single electron pt 1 to 20')
+)
+
+
+# Path and EndPath definitions
+process.generation_step = cms.Path(process.pgen)
+process.simulation_step = cms.Path(process.psim)
+process.genfiltersummary_step = cms.EndPath(process.genFilterSummary)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.RAWSIMoutput_step = cms.EndPath(process.RAWSIMoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.generation_step,process.genfiltersummary_step,process.simulation_step,process.endjob_step,process.RAWSIMoutput_step)
+from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
+associatePatAlgosToolsTask(process)
+# filter all path with the production filter sequence
+for path in process.paths:
+	getattr(process,path)._seq = process.generator * getattr(process,path)._seq 
+
+# customisation of the process.
+
+# Automatic addition of the customisation function from Configuration.Generator.RandomSeed_cfi
+from Configuration.Generator.RandomSeed_cfi import customizeRandomSeed 
+
+#call to customisation function customizeRandomSeed imported from Configuration.Generator.RandomSeed_cfi
+process = customizeRandomSeed(process)
+
+# End of customisation functions
+
+# Customisation from command line
+
+# Add early deletion of temporary data products to reduce peak memory need
+from Configuration.StandardSequences.earlyDeleteSettings_cff import customiseEarlyDelete
+process = customiseEarlyDelete(process)
+# End adding early deletion
---
+++ flat_pt_electron/gen_sim/batch.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Set those variables:
+config_path=/afs/cern.ch/work/r/rchudasa/private/ruchi_reco_2018/CMSSW_10_3_2/src/Configuration/Generator/python/flat_pt_electron/gen_sim/SingleGammaFlatPt_pythia8_cfi_GEN_SIM.py
+#input_path=/afs/cern.ch/work/r/rchudasa/private/ruchi_reco_2018/CMSSW_10_3_2/src/mc_reconstruction/reco/qed_reco/input_filelist.txt
+cmssw_path=/afs/cern.ch/work/r/rchudasa/private/ruchi_reco_2018/CMSSW_10_3_2/src/
+output_path=/eos/cms/store/group/phys_diffraction/lbyl_2018/mc_flat_pt_electron/gen_sim/
+outfilename="flatpt_electron_gen_sim_${1}.root"
+
+#----------------------------------------------------------------------------
+export XRD_NETWORKSTACK=IPv4
+export CMSSWVER="CMSSW_10_3_2"
+export SCRAM_ARCH="slc6_amd64_gcc700"
+
+cd $cmssw_path
+cmsenv
+
+source /afs/cern.ch/cms/cmsset_default.sh
+eval `scramv1 runtime -sh`
+#edmPluginRefresh -p ../lib/$SCRAM_ARCH
+
+echo "Job running"
+#echo "awk -v awklinefrom=$((${1}+1)) -v awklineto=$((${1}+1)) 'NR==awklinefrom,NR==awklineto' ${input_path}"
+
+#infilename=`awk -v awklinefrom=$((${1}+1)) -v awklineto=$((${1}+1)) 'NR==awklinefrom,NR==awklineto' ${input_path}`
+#echo "Input files: ${infilename}"
+#echo "Executing command:cmsRun $config_path outputFile=$outfilename inputFiles=$infilename"
+echo "Executing command:cmsRun $config_path outputFile=$outfilename"
+
+#cmsRun $config_path outputFile=$outfilename inputFiles=$infilename
+cmsRun $config_path outputFile=$outfilename 
+xrdcp -N -v $outfilename root://eoscms.cern.ch/$output_path
+rm $outfilename
