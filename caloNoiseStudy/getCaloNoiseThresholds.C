f25b4f34b38e920ffd94ccfcdbbe4379fda2c87c
jniedzie <jeremi.niedziela@cern.ch>
2019-05-07 08:48:25 +0200

    Updating macro for noise thresholds finding

--- caloNoiseStudy/getCaloNoiseThresholds.C
+++ caloNoiseStudy/getCaloNoiseThresholds.C
@@ -1,44 +1,40 @@
 #include "CaloNoiseHelpers.h"
 
-void getCaloNoiseThresholds()
+void getCaloNoiseThresholds(const char* infileName)
 {
+  TFile *inFile = TFile::Open(infileName);
+  if(!inFile){
+    cout<<"File not found:"<<infileName<<endl;
+    exit(0);
+  }
+  TH1D *energyHist[nDets];
+  double thresholds[nDets];
   
   // Find noise thresholds
   for(int iDet=0;iDet<nDets;iDet++){
+    energyHist[iDet] = (TH1D*)inFile->Get(Form("energyHist%s",detNames[iDet].c_str()));
+    
     // Normalize histograms by number of entries
     energyHist[iDet]->Scale(1/energyHist[iDet]->GetEntries());
     
-    double histIntegral = energyHist[iDet]->Integral(energyHist[iDet]->GetXaxis()->FindFixBin(0.0),
-                                                     energyHist[iDet]->GetXaxis()->FindFixBin(10.0));
+    double histIntegral = energyHist[iDet]->Integral(1, energyHist[iDet]->GetNbinsX()+1);
     
-    double threshold;
-    
-    for(threshold=0; threshold<10.0; threshold+=0.01){
-      double currentIntegral = energyHist[iDet]->Integral(energyHist[iDet]->GetXaxis()->FindFixBin(0.0),
-                                                          energyHist[iDet]->GetXaxis()->FindFixBin(threshold));
-      
+    for(int iBin=1; iBin<=energyHist[iDet]->GetNbinsX(); iBin++){
+      double currentIntegral = energyHist[iDet]->Integral(1,iBin);
+      thresholds[iDet] = energyHist[iDet]->GetBinCenter(iBin);
       if(currentIntegral >= 0.99*histIntegral) break;
     }
     
-    cout<<"Threshold for "<<detNames[iDet]<<": "<<threshold<<" GeV"<<endl;
+    cout<<"Threshold for "<<detNames[iDet]<<": "<<thresholds[iDet]<<" GeV"<<endl;
   }
-  
   
   // Draw histograms and save the output
-  auto inputCanvas  = new TCanvas("inputCanvas", "inputCanvas",   800, 1200);
-  auto outputCanvas = new TCanvas("outputCanvas", "outputCanvas", 800, 1200);
-  
-  inputCanvas->Divide(2,3);
-  outputCanvas->Divide(2,3);
-  
-  int iPad=1;
-  for(auto &[varName, a, b, c] : inputVarParams){
-    inputCanvas->cd(iPad++);
-    inputHists[varName]->Draw();
-  }
+  auto thresholdsCanvas = new TCanvas("thresholdsCanvas", "thresholdsCanvas", 800, 1200);
+
+  thresholdsCanvas->Divide(2,3);
   
   for(int iDet=0;iDet<nDets;iDet++){
-    outputCanvas->cd(iDet+1);
+    thresholdsCanvas->cd(iDet+1);
     gPad->SetLogy();
     energyHist[iDet]->Draw();
   }
