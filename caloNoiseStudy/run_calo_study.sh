cdd83f83bf0e6841c3c8377ace081029867470fa
Jeremi Niedziela <jeremi.niedziela@cern.ch>
2019-07-15 15:02:32 +0200

    Updating paths for calo noise study

--- caloNoiseStudy/run_calo_study.sh
+++ caloNoiseStudy/run_calo_study.sh
@@ -3,7 +3,7 @@
 # Set those variables:
 exec_path=/afs/cern.ch/work/j/jniedzie/private/LightByLight2018/caloNoiseStudy/getCaloNoiseHistograms
 input_path=/afs/cern.ch/work/j/jniedzie/private/LightByLight2018/caloNoiseStudy/input_filelist.txt
-output_path=/eos/cms/store/group/phys_heavyions/jniedzie/EmptyBx/noiseStudyOutput/
+output_path=/eos/cms/store/cmst3/group/lightbylight/EmptyBx_HIForest/noiseStudyOutput/
 
 #----------------------------------------------------------------------------
 source /cvmfs/sft.cern.ch/lcg/releases/LCG_94/gcc/8.2.0/x86_64-slc6/setup.sh
@@ -15,5 +15,6 @@ infilename=`awk -v awklinefrom=$((${1}+1)) -v awklineto=$((${1}+1)) 'NR==awkline
 echo "Input files: ${infilename}"
 outfilename="noiseStudyOutput_${1}.root"
 
+mkdir -p $output_path
 cd $output_path
 $exec_path /eos/cms/$infilename caloNoiseHists_${1}.root
