188256fb24a2e023bb0f6f183710df36f3efbf67
Jeremi Niedziela <jeremi.niedziela@cern.ch>
2019-07-29 11:00:23 +0200

    Updating condor scripts for new names of efficiencies apps

--- analysis/runEfficienciesQED_MC.sh
+++ analysis/runEfficienciesQED_MC.sh
@@ -4,6 +4,6 @@ configPath="/afs/cern.ch/work/j/jniedzie/private/LightByLight2018/analysis/confi
 
 inputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/mc_qed/ntuples_sc/reco_mc_qed_sc/reco_mc_qed_sc/190723_061308/0000/HiForestAOD_mc_${1}.root"
 
-output="/eos/cms/store/group/phys_diffraction/lbyl_2018/mc_qed/efficiency_sc/efficienciesData_${1}.root"
+output="/eos/cms/store/group/phys_diffraction/lbyl_2018/mc_qed/efficiency_sc/efficienciesQED_${1}.root"
 
-/afs/cern.ch/work/j/jniedzie/private/LightByLight2018/analysis/getEfficienciesData $configPath $inputPath $output
+/afs/cern.ch/work/j/jniedzie/private/LightByLight2018/analysis/getEfficienciesQED $configPath $inputPath $output
