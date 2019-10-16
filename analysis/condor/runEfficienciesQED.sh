398daf05a93da319f300d295449280c4b9d73115
jniedzie <jeremi.niedziela@cern.ch>
2019-10-16 14:49:47 +0200

    Updating path to data ntuples with L1 branches.

--- analysis/condor/runEfficienciesQED.sh
+++ analysis/condor/runEfficienciesQED.sh
@@ -2,7 +2,8 @@
 
 configPath="/afs/cern.ch/work/j/jniedzie/private/LightByLight2018/analysis/configs/efficiencies.md"
 
-inputPathData="/eos/cms/store/group/phys_diffraction/lbyl_2018/HIForward_Reco/ntuples/ntuples_data_lbl/ntuples_data_lbl/190723_062214/0000/HiForestAOD_AOD_${1}.root"
+#inputPathData="/eos/cms/store/group/phys_diffraction/lbyl_2018/HIForward_Reco/ntuples/ntuples_data_lbl/ntuples_data_lbl/190723_062214/0000/HiForestAOD_AOD_${1}.root"
+inputPathData="/eos/cms/store/group/phys_diffraction/lbyl_2018/HIForward_Reco/ntuples/ntuples_data_l1_branches/ntuples_data_l1_branches/191008_125700/0000/HiForestAOD_${1}.root"
 
 inputPathQED_SC="/eos/cms/store/group/phys_diffraction/lbyl_2018/mc_qed/ntuples_sc/reco_mc_qed_sc/reco_mc_qed_sc/190723_061308/0000/HiForestAOD_mc_${1}.root"
 
