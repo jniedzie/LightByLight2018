fc1cd65d86709cb0498d8519f5150e2862194609
jniedzie <jeremi.niedziela@cern.ch>
2019-11-18 10:18:39 +0100

    Adding macro to select events passing trigger

---
+++ analysis/ntuples/triggerSelect.C
@@ -0,0 +1,67 @@
+#include "TTree.h"
+#include "TFile.h"
+
+#include <iostream>
+
+using namespace std;
+
+const char* inFileName  = "ntuples_mc_qed_sc_withL1_merged.root";
+//const char* inFileName  = "ntuples_mc_qed_sc_withL1_test.root";
+const char* outFileName = "ntuples_mc_qed_withSingleEG3.root";
+//const char* outFileName = "ntuples_mc_qed_withSingleEG3_test.root";
+
+void triggerSelect()
+{
+  cout<<"Starting conversion"<<endl;
+  TFile *inFile       = TFile::Open(inFileName);
+  TTree *inTreeHLT    = (TTree*)inFile->Get("hltanalysis/HltTree");
+  TTree *inTreeL1     = (TTree*)inFile->Get("l1object/L1UpgradeFlatTree");
+  TTree *inTreeEvent  = (TTree*)inFile->Get("ggHiNtuplizer/EventTree");
+  cout<<"Trees opened"<<endl;
+  int hasTrigger;
+  inTreeHLT->SetBranchAddress("HLT_HIUPC_SingleEG3_NotMBHF2AND_v1", &hasTrigger);
+  
+  int nWithTrigger = 0;
+  
+  TFile *outFile = new TFile(outFileName, "recreate");
+  outFile->cd();
+  
+  TDirectory *dirHLT    = outFile->mkdir("hltanalysis");
+  TDirectory *dirL1     = outFile->mkdir("l1object");
+  TDirectory *dirEvent  = outFile->mkdir("ggHiNtuplizer");
+  
+  cout<<"Cloning trees"<<endl;
+  TTree *outTreeHLT   = inTreeHLT->CloneTree(0);
+  TTree *outTreeL1    = inTreeL1->CloneTree(0);
+  TTree *outTreeEvent = inTreeEvent->CloneTree(0);
+  cout<<"Resetting trees"<<endl;
+  outTreeHLT->Reset();
+  outTreeL1->Reset();
+  outTreeEvent->Reset();
+  cout<<"Processing events"<<endl;
+  for(int iEvent=0; iEvent<inTreeHLT->GetEntries(); iEvent++){
+    if(iEvent%10000==0) cout<<"Processed events: "<<iEvent<<endl;
+    
+    inTreeHLT->GetEntry(iEvent);
+    inTreeL1->GetEntry(iEvent);
+    inTreeEvent->GetEntry(iEvent);
+    
+    if(hasTrigger){
+      nWithTrigger++;
+      outTreeHLT->Fill();
+      outTreeL1->Fill();
+      outTreeEvent->Fill();
+    }
+  }
+  
+  cout<<nWithTrigger<<"/"<<inTreeHLT->GetEntries()<<endl;
+  
+  dirHLT->cd();
+  outTreeHLT->Write();
+  dirL1->cd();
+  outTreeL1->Write();
+  dirEvent->cd();
+  outTreeEvent->Write();
+  
+  outFile->Close();
+}
