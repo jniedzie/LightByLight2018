e05bee0d72ab83356c89627e81002d22c9464713
jniedzie <jeremi.niedziela@cern.ch>
2020-01-16 14:29:16 +0100

    Updating calo noise macro

--- caloNoiseStudy/getCaloNoiseThresholds.C
+++ caloNoiseStudy/getCaloNoiseThresholds.C
@@ -1,117 +1,39 @@
 #include "CaloNoiseHelpers.h"
 
-TH1D* GetEEhist(TFile *inFile, double etaMin, double etaMax);
-TH1D* GetHFphist(TFile *inFile, double etaMin);
-TH1D* GetHFmhist(TFile *inFile, double etaMax);
-
-void getCaloNoiseThresholds(const char* infileName)
+void getCaloNoiseThresholds()
 {
-  TFile *inFile = TFile::Open(infileName);
-  if(!inFile){
-    cout<<"File not found:"<<infileName<<endl;
-    exit(0);
-  }
-  TH1D *energyHist[nDets];
-  double thresholds[nDets];
+  TFile *inFile = TFile::Open("noiseStudyOutput_merged.root");
+  auto canvas = new TCanvas("canvas", "canvas", 800, 1200);
+  canvas->Divide(2,3);
   
   // Find noise thresholds
   for(int iDet=0;iDet<nDets;iDet++){
-    if(iDet == kEE){ // EE is very noisy, so we need to remove some eta regions
-      energyHist[iDet] = GetEEhist(inFile, -2.3, 2.3);
-    }
-//    else if(iDet == kHFp){
-//      energyHist[iDet] = GetHFphist(inFile, 3.4);
-//    }
-//    else if(iDet == kHFm){
-//      energyHist[iDet] = GetHFmhist(inFile,-3.4);
-//    }
-    else{
-      energyHist[iDet] = (TH1D*)inFile->Get(Form("energyHist%s",detNames[iDet].c_str()));
-    }
-    if(!energyHist[iDet]){
-      continue;
-    }
-    
     // Normalize histograms by number of entries
-    energyHist[iDet]->Scale(1/energyHist[iDet]->GetEntries());
+    auto hist = (TH1D*)inFile->Get(("energyHist"+detNames[iDet]).c_str());
     
-    double histIntegral = energyHist[iDet]->Integral(1, energyHist[iDet]->GetNbinsX()+1);
+    hist->Scale(1/hist->GetEntries());
     
-    for(int iBin=1; iBin<=energyHist[iDet]->GetNbinsX(); iBin++){
-      double currentIntegral = energyHist[iDet]->Integral(1,iBin);
-      thresholds[iDet] = energyHist[iDet]->GetBinCenter(iBin);
+    double histIntegral = hist->Integral(hist->GetXaxis()->FindFixBin(0.0),
+                                                     hist->GetXaxis()->FindFixBin(10.0));
+    
+    double threshold;
+    
+    for(threshold=0; threshold<10.0; threshold+=0.01){
+      double currentIntegral = hist->Integral(hist->GetXaxis()->FindFixBin(0.0),
+                                                          hist->GetXaxis()->FindFixBin(threshold));
+      
       if(currentIntegral >= 0.99*histIntegral) break;
     }
     
-    cout<<"Threshold for "<<detNames[iDet]<<": "<<thresholds[iDet]<<" GeV"<<endl;
-  }
-  
-  // Draw histograms and save the output
-  auto thresholdsCanvas = new TCanvas("thresholdsCanvas", "thresholdsCanvas", 800, 1200);
-
-  thresholdsCanvas->Divide(2,3);
-  
-  for(int iDet=0;iDet<nDets;iDet++){
-    if(!energyHist[iDet]) continue;
-    thresholdsCanvas->cd(iDet+1);
+    cout<<"Threshold for "<<detNames[iDet]<<": "<<threshold<<" GeV"<<endl;
+    
+    TLine thresholdLine(threshold, 0, threshold, 1);
+    thresholdLine.SetLineColor(kMagenta+2);
+    thresholdLine.SetLineWidth(1.0);
+    
+    canvas->cd(iDet+1);
     gPad->SetLogy();
-    energyHist[iDet]->SetLineColor(kViolet+3);
-    
-    if(iDet == kHFm)      energyHist[iDet]->SetTitle("Leading tower energy HF-");
-    else if(iDet == kHFp) energyHist[iDet]->SetTitle("Leading tower energy HF+");
-    else{
-      energyHist[iDet]->SetTitle(Form("Leading tower energy %s", detNames[iDet].c_str()));
-    }
-    energyHist[iDet]->Draw();
-    energyHist[iDet]->GetXaxis()->SetRangeUser(0, 10);
-    energyHist[iDet]->GetXaxis()->SetTitle("Leading tower E (GeV)");
-    
-    TLine *line = new TLine(thresholds[iDet], energyHist[iDet]->GetMinimum(),
-                            thresholds[iDet], energyHist[iDet]->GetMaximum());
-    line->SetLineColor(kGreen+3);
-    line->Draw("same");
-    
-    TLatex *text = new TLatex(4.5,
-                              1e-2,
-                              Form("Threshold: %.1f (GeV)", round(10*thresholds[iDet])/10.));
-    
-    text->SetTextColor(kGreen+3);
-    text->Draw("same");
-    
+    hist->Draw();
+    thresholdLine.Draw();
   }
-  
-  thresholdsCanvas->SaveAs("noise_thresholds.pdf");
-}
-
-TH1D* GetEEhist(TFile *inFile, double etaMin, double etaMax)
-{
-  TH2D *noiseVsEtaEE = (TH2D*)inFile->Get("energyHistEEVsEta");
-  TH1D *noiseEEwithCut = noiseVsEtaEE->ProjectionY("projEE",
-                                                   noiseVsEtaEE->GetXaxis()->FindFixBin(etaMin),
-                                                   noiseVsEtaEE->GetXaxis()->FindFixBin(etaMax));
-  
-  noiseEEwithCut->SetTitle("energyHistEE");
-  
-  return noiseEEwithCut;
-}
-
-TH1D* GetHFphist(TFile *inFile, double etaMin)
-{
-  TH2D *noiseVsEtaHF = (TH2D*)inFile->Get("energyHistHFpVsEta");
-  
-  TH1D *noiseHFwithCut = noiseVsEtaHF->ProjectionY("projHFpos",
-                                                   noiseVsEtaHF->GetXaxis()->FindFixBin(etaMin),
-                                                   noiseVsEtaHF->GetNbinsX());
-  return noiseHFwithCut;
-}
-
-TH1D* GetHFmhist(TFile *inFile, double etaMax)
-{
-  TH2D *noiseVsEtaHF = (TH2D*)inFile->Get("energyHistHFmVsEta");
-  
-  TH1D *noiseHFwithCut = noiseVsEtaHF->ProjectionY("projHFneg",
-                                                   1,
-                                                   noiseVsEtaHF->GetXaxis()->FindFixBin(etaMax));
-  
-  return noiseHFwithCut;
 }
