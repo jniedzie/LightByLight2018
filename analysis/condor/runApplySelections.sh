dd668987b54fea66135e430dd80949ed7c983bcf
Jeremi Niedziela <jeremi.niedziela@cern.ch>
2020-01-17 14:53:49 +0100

    Updating script for applySelection on condor

--- analysis/condor/runApplySelections.sh
+++ analysis/condor/runApplySelections.sh
@@ -2,8 +2,14 @@
 
 configPath="/afs/cern.ch/work/j/jniedzie/private/LightByLight2018/analysis/configs/efficiencies.md"
 
-inputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/data_all_withSingleEG3/ntuples/data_HiForestAOD_withSingleEG3_${1}.root"
+ls /afs/cern.ch/work/j/jniedzie/private/LightByLight2018/analysis/input_list.txt
 
-output="/eos/cms/store/group/phys_diffraction/lbyl_2018/data_forTriggerEff/data_HiForestAOD_forRecoEff_${1}.root"
+inputPath=`sed "${1}q;d" /afs/cern.ch/work/j/jniedzie/private/LightByLight2018/analysis/input_list.txt`
+echo "Input path: ${inputPath}"
+
+outputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/data_forTriggerEff"
+mkdir -p $outputPath
+
+output="${outputPath}/data_HiForestAOD_forTriggerEff_${1}.root"
 
 /afs/cern.ch/work/j/jniedzie/private/LightByLight2018/analysis/applySelections $configPath $inputPath $output
