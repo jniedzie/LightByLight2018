52035715783a4ab2bd20d53ee880c92f70d8eb38
Jeremi Niedziela <jeremi.niedziela@cern.ch>
2020-01-21 15:13:19 +0100

    Updataing condor scripts for applying selections

--- analysis/condor/runApplySelections.sh
+++ analysis/condor/runApplySelections.sh
@@ -2,15 +2,16 @@
 
 configPath="/afs/cern.ch/work/j/jniedzie/private/LightByLight2018/analysis/configs/efficiencies.md"
 
-#inputPath=`sed "${1}q;d" /afs/cern.ch/work/j/jniedzie/private/LightByLight2018/analysis/input_list.txt`
-inputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/mc_qed/ntuples_superchic_1034/ntuples_sc_1034/ntuples_sc_1034/191113_105005/0000/HiForestAOD_LbyL_full_sample_lbyl_reco_${1}.root"
-
-echo "Input path: ${inputPath}"
-
-#outputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/data_forTriggerEff"
-outputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/mc_qed_sc_forTriggerEff"
+# for the data:
+inputPath=`sed "${1}q;d" /afs/cern.ch/work/j/jniedzie/private/LightByLight2018/analysis/input_list.txt`
+outputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/data_forTriggerEff"
 mkdir -p $outputPath
-#output="${outputPath}/data_HiForestAOD_forTriggerEff_${1}.root"
-output="${outputPath}/mc_qed_sc_forTriggerEff_${1}.root"
+output="${outputPath}/data_HiForestAOD_forTriggerEff_${1}.root"
+
+# for SC MC QED
+#inputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/mc_qed/ntuples_superchic_1034/ntuples_sc_1034/ntuples_sc_1034/191113_105005/0000/HiForestAOD_LbyL_full_sample_lbyl_reco_${1}.root"
+#outputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/mc_qed_sc_forTriggerEff"
+#mkdir -p $outputPath
+#output="${outputPath}/mc_qed_sc_forTriggerEff_${1}.root"
 
 /afs/cern.ch/work/j/jniedzie/private/LightByLight2018/analysis/applySelections $configPath $inputPath $output
