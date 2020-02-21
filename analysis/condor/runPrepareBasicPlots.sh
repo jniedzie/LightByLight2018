61f7d8d695bc8c7698b9c8cbcc027b589a9da69a
Jeremi Niedziela <jeremi.niedziela@cern.ch>
2020-02-21 14:03:01 +0100

    Updating condor scripts

--- analysis/condor/runPrepareBasicPlots.sh
+++ analysis/condor/runPrepareBasicPlots.sh
@@ -14,21 +14,25 @@ then
   outputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/analysis/basicPlots/basicPlots_data_CHE"
   sampleName="Data"
 elif [ $2 -eq 1 ]
+then
  # MC QED SC
   inputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/mc_qed/ntuples_superchic_1034/ntuples_sc_1034/ntuples_sc_1034/191113_105005/0000/HiForestAOD_LbyL_full_sample_lbyl_reco_${1}.root"
   outputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/analysis/basicPlots/basicPlots_mc_qed_sc_CHE"
   sampleName="QED_SC"
 elif [ $2 -eq 2 ]
+then
   # MC QED SL
 #  inputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/mc_qed/ntuples_superchic_1034/ntuples_sc_1034/ntuples_sc_1034/191113_105005/0000/HiForestAOD_LbyL_full_sample_lbyl_reco_${1}.root"
   outputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/analysis/basicPlots/basicPlots_mc_qed_sl_CHE"
   sampleName="QED_SL"
 elif [ $2 -eq 3 ]
+then
    # MC CEP SC
   inputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/mc_cep/ntuples_1034/ntuples_cep_1034/ntuples_cep_1034/200211_054704/0000/HiForestAOD_cep_${1}.root"
   outputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/analysis/basicPlots/basicPlots_mc_cep_sc_CHE"
   sampleName="CEP"
 elif [ $2 -eq 4 ]
+then
     # MC LbL SC
   inputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/mc_lbl/ntuples_1034/ntuples_lbl_1034/ntuples_lbl_1034/200207_114802/0000/HiForestAOD_LbyL_${1}.root"
   outputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/analysis/basicPlots/basicPlots_mc_lbl_sc_CHE"
