5233d7045f52028d9862ab2ef8f47a423091719e
Jeremi Niedziela <jeremi.niedziela@cern.ch>
2020-02-21 12:20:13 +0100

    Updating condor scripts

--- analysis/condor/runApplySelections.sh
+++ analysis/condor/runApplySelections.sh
@@ -10,26 +10,39 @@ outputPathHFveto=""
 outputPathExclusivity=""
 outputPathLbLsignal=""
 outputPathQEDsignal=""
+outputPathLowAco=""
+outputPathHighAco=""
 
-# data
-if [ $2 -eq 0 ]
+
+if [ $2 -eq 0 ] # data
 then
   inputPath=`sed "${1}q;d" /afs/cern.ch/work/j/jniedzie/private/LightByLight2018/analysis/input_list.txt`
-  outputPathReco="/eos/cms/store/group/phys_diffraction/lbyl_2018/data_forRecoEff"
-  outputPathTrigger="/eos/cms/store/group/phys_diffraction/lbyl_2018/data_forTriggerEff"
-  outputPathHFveto="/eos/cms/store/group/phys_diffraction/lbyl_2018/data_forHFvetoEff"
-  outputPathExclusivity="/eos/cms/store/group/phys_diffraction/lbyl_2018/data_forExclusivityEff"
-  outputPathLbLsignal="/eos/cms/store/group/phys_diffraction/lbyl_2018/data_forLbLsignal"
-  outputPathQEDsignal="/eos/cms/store/group/phys_diffraction/lbyl_2018/data_forQEDsignal"
-else
- # MC
-  inputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/mc_qed/ntuples_superchic_1034/ntuples_sc_1034/ntuples_sc_1034/191113_105005/0000/HiForestAOD_LbyL_full_sample_lbyl_reco_${1}.root"
-  outputPathReco="/eos/cms/store/group/phys_diffraction/lbyl_2018/mc_qed_sc_forRecoEff"
-  outputPathTrigger="/eos/cms/store/group/phys_diffraction/lbyl_2018/mc_qed_sc_forTriggerEff"
-  outputPathHFveto="/eos/cms/store/group/phys_diffraction/lbyl_2018/mc_qed_sc_forHFvetoEff"
-  outputPathExclusivity="/eos/cms/store/group/phys_diffraction/lbyl_2018/mc_qed_sc_forExclusivityEff"
-  outputPathLbLsignal="/eos/cms/store/group/phys_diffraction/lbyl_2018/mc_qed_sc_forLbLsignal"
-  outputPathQEDsignal="/eos/cms/store/group/phys_diffraction/lbyl_2018/mc_qed_sc_forQEDsignal"
+  outputPathReco="/eos/cms/store/group/phys_diffraction/lbyl_2018/skimmed_ntuples/data_forRecoEff_tmp"
+  outputPathTrigger="/eos/cms/store/group/phys_diffraction/lbyl_2018/skimmed_ntuples/data_forTriggerEff_tmp"
+  outputPathHFveto="/eos/cms/store/group/phys_diffraction/lbyl_2018/skimmed_ntuples/data_forHFvetoEff_tmp"
+  outputPathExclusivity="/eos/cms/store/group/phys_diffraction/lbyl_2018/skimmed_ntuples/data_forExclusivityEff_tmp"
+  outputPathLbLsignal="/eos/cms/store/group/phys_diffraction/lbyl_2018/skimmed_ntuples/data_forLbLsignal_noHFcheck"
+  outputPathQEDsignal="/eos/cms/store/group/phys_diffraction/lbyl_2018/skimmed_ntuples/data_forQEDsignal_tmp"
+elif [ $2 -eq 1 ] # MC
+  # QED
+#  inputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/mc_qed/ntuples_superchic_1034/ntuples_sc_1034/ntuples_sc_1034/191113_105005/0000/HiForestAOD_LbyL_full_sample_lbyl_reco_${1}.root"
+  
+  # LbL
+#  inputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/mc_lbl/ntuples_1034/ntuples_lbl_1034/ntuples_lbl_1034/200207_114802/0000/HiForestAOD_LbyL_${1}.root"
+  
+  # CEP
+  inputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/mc_cep/ntuples_1034/ntuples_cep_1034/ntuples_cep_1034/200211_054704/0000/HiForestAOD_cep_${1}.root"
+  
+  outputPathReco="/eos/cms/store/group/phys_diffraction/lbyl_2018/mc_cep_sc_forRecoEff"
+  outputPathTrigger="/eos/cms/store/group/phys_diffraction/lbyl_2018/mc_cep_sc_forTriggerEff"
+  outputPathHFveto="/eos/cms/store/group/phys_diffraction/lbyl_2018/mc_cep_sc_forHFvetoEff"
+  outputPathExclusivity="/eos/cms/store/group/phys_diffraction/lbyl_2018/mc_cep_sc_forExclusivityEff"
+  outputPathLbLsignal="/eos/cms/store/group/phys_diffraction/lbyl_2018/mc_cep_sc_forLbLsignal"
+  outputPathQEDsignal="/eos/cms/store/group/phys_diffraction/lbyl_2018/mc_cep_sc_forQEDsignal"
+elif [ $2 -eq 2 ] # Data passing LbL selections
+  inputPath=`sed "${1}q;d" /afs/cern.ch/work/j/jniedzie/private/LightByLight2018/analysis/input_list.txt`
+  outputPathLowAco="/eos/cms/store/group/phys_diffraction/lbyl_2018/skimmed_ntuples/data_passingLbL_lowAco"
+  outputPathHighAco="/eos/cms/store/group/phys_diffraction/lbyl_2018/skimmed_ntuples/data_passingLbL_highAco"
 fi
 
 mkdir -p $outputPathReco
@@ -38,6 +51,8 @@ mkdir -p $outputPathHFveto
 mkdir -p $outputPathExclusivity
 mkdir -p $outputPathLbLsignal
 mkdir -p $outputPathQEDsignal
+mkdir -p $outputPathLowAco
+mkdir -p $outputPathHighAco
 
 outputReco="${outputPathReco}/ntuples_forRecoEff_${1}.root"
 outputTrigger="${outputPathTrigger}/ntuples_forTriggerEff_${1}.root"
@@ -45,6 +60,9 @@ outputHFveto="${outputPathHFveto}/ntuples_forHFvetoEff_${1}.root"
 outputExclusivity="${outputPathExclusivity}/ntuples_forExclusivityEff_${1}.root"
 outputLbLsignal="${outputPathLbLsignal}/ntuples_forLbLsignal_${1}.root"
 outputQEDsignal="${outputPathQEDsignal}/ntuples_forQEDsignal_${1}.root"
+outputLowAco="${outputPathLowAco}/ntuples_passingLbL_lowAco_${1}.root"
+outputHighAco="${outputPathHighAco}/ntuples_passingLbL_highAco_${1}.root"
+
 
 echo "Config: ${configPath}"
 echo "Input: ${inputPath}"
@@ -54,5 +72,14 @@ echo "Output HF veto: ${outputHFveto}"
 echo "Output exclusivity: ${outputExclusivity}"
 echo "Output LbL signal: ${outputLbLsignal}"
 echo "Output QED signal: ${outputQEDsignal}"
+echo "Output low aco: ${outputLowAco}"
+echo "Output high aco: ${outputHighAco}"
 
-/afs/cern.ch/work/j/jniedzie/private/LightByLight2018/analysis/applySelections $configPath $inputPath $outputReco $outputTrigger $outputHFveto $outputExclusivity $outputLbLsignal $outputQEDsignal
+if [ $2 -eq 0 ] # data
+then
+  /afs/cern.ch/work/j/jniedzie/private/LightByLight2018/analysis/applySelections $configPath $inputPath $outputReco $outputTrigger $outputHFveto $outputExclusivity $outputLbLsignal $outputQEDsignal
+elif [ $2 -eq 1 ] # MC
+  /afs/cern.ch/work/j/jniedzie/private/LightByLight2018/analysis/applySelections $configPath $inputPath $outputReco $outputTrigger $outputHFveto $outputExclusivity $outputLbLsignal $outputQEDsignal
+elif [ $2 -eq 2 ] # Data passing LbL selections
+  /afs/cern.ch/work/j/jniedzie/private/LightByLight2018/analysis/applySelections $inputPath $outputLowAco $outputHighAco
+fi
--- analysis/condor/runPrepareBasicPlots.sh
+++ analysis/condor/runPrepareBasicPlots.sh
@@ -10,11 +10,23 @@ outputPath=""
 if [ $2 -eq 0 ]
 then
   inputPath=`sed "${1}q;d" /afs/cern.ch/work/j/jniedzie/private/LightByLight2018/analysis/input_list.txt`
-  outputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/basicPlots_data_etaDepNEE"
-else
- # MC
+  outputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/analysis/basicPlots/basicPlots_data_CHE"
+elif [ $2 -eq 1 ]
+ # MC QED SC
   inputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/mc_qed/ntuples_superchic_1034/ntuples_sc_1034/ntuples_sc_1034/191113_105005/0000/HiForestAOD_LbyL_full_sample_lbyl_reco_${1}.root"
-  outputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/basicPlots_mc_etaDepNEE"
+  outputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/analysis/basicPlots/basicPlots_mc_qed_sc_CHE"
+elif [ $2 -eq 2 ]
+  # MC QED SL
+#  inputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/mc_qed/ntuples_superchic_1034/ntuples_sc_1034/ntuples_sc_1034/191113_105005/0000/HiForestAOD_LbyL_full_sample_lbyl_reco_${1}.root"
+  outputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/analysis/basicPlots/basicPlots_mc_qed_sl_CHE"
+elif [ $2 -eq 3 ]
+   # MC CEP SC
+  inputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/mc_cep/ntuples_1034/ntuples_cep_1034/ntuples_cep_1034/200211_054704/0000/HiForestAOD_cep_${1}.root"
+  outputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/analysis/basicPlots/basicPlots_mc_cep_sc_CHE"
+elif [ $2 -eq 4 ]
+    # MC LbL SC
+  inputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/mc_lbl/ntuples_1034/ntuples_lbl_1034/ntuples_lbl_1034/200207_114802/0000/HiForestAOD_LbyL_${1}.root"
+  outputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/analysis/basicPlots/basicPlots_mc_lbl_sc_CHE"
 fi
 
 mkdir -p $outputPath
