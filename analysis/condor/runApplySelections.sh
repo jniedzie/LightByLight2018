95875bae4480e206ae8a63a097fcfa9178b339b6
Jeremi Niedziela <jeremi.niedziela@cern.ch>
2020-02-27 16:04:42 +0100

    Updating condor scripts

--- analysis/condor/runApplySelections.sh
+++ analysis/condor/runApplySelections.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 configPath="/afs/cern.ch/work/j/jniedzie/private/LightByLight2018/analysis/configs/applySelections.md"
+basePath="/eos/cms/store/group/phys_diffraction/lbyl_2018"
+
 
 # for the data:
 inputPath=""
@@ -60,10 +62,24 @@ then
   outputPathHighAco="/eos/cms/store/group/phys_diffraction/lbyl_2018/skimmed_ntuples/data_passingLbL_highAco"
   mkdir -p $outputPathLowAco
   mkdir -p $outputPathHighAco
-elif [ $2 -eq 3 ] # Data passing LbL selections
+elif [ $2 -eq 3 ] # Loose selections
 then
-  inputPath=`sed "${1}q;d" /afs/cern.ch/work/j/jniedzie/private/LightByLight2018/analysis/input_list.txt`
-  outputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/skimmed_ntuples/data_loose_selections"
+# data
+#  inputPath=`sed "${1}q;d" /afs/cern.ch/work/j/jniedzie/private/LightByLight2018/analysis/input_list.txt`
+#  outputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/skimmed_ntuples/data_loose_selections"
+  
+# QED SC
+  inputPath="${basePath}/mc_qed/ntuples_superchic_1034/ntuples_sc_1034/ntuples_sc_1034/191113_105005/0000/HiForestAOD_LbyL_full_sample_lbyl_reco_${1}.root"
+  outputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/skimmed_ntuples/mc_qed_sc_loose_selections"
+  
+# CEP SC
+#  inputPath="${basePath}/mc_cep/ntuples_1034/ntuples_cep_1034/ntuples_cep_1034/200211_054704/0000/HiForestAOD_cep_${1}.root"
+#  outputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/skimmed_ntuples/mc_cep_sc_loose_selections"
+  
+# LbL SC
+#  inputPath="${basePath}/mc_lbl/ntuples_1034/ntuples_lbl_1034/ntuples_lbl_1034/200207_114802/0000/HiForestAOD_LbyL_${1}.root"
+#  outputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/skimmed_ntuples/mc_lbl_sc_loose_selections"
+  
   mkdir -p $outputPath
 fi
 
--- analysis/condor/runPrepareBasicPlots.sh
+++ analysis/condor/runPrepareBasicPlots.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
 configPath=""
+suffix=""
 
 if [ $3 -eq 0 ]
 then
-  configPath="/afs/cern.ch/work/j/jniedzie/private/LightByLight2018/analysis/configs/efficiencies.md"
+# default
+  configPath="/afs/cern.ch/work/j/jniedzie/private/LightByLight2018/analysis/configs/preparePlots_default.md"
 elif [ $3 -eq 1 ]
 then
-  configPath="/afs/cern.ch/work/j/jniedzie/private/LightByLight2018/analysis/configs/efficiencies_20GeVHF.md"
+# HF thresholds increased to 20 GeV
+  configPath="/afs/cern.ch/work/j/jniedzie/private/LightByLight2018/analysis/configs/preparePlots_HF_20GeV.md"
+  suffix="_HF_20GeV"
 fi
 
 # for the data:
@@ -15,68 +19,36 @@ inputPath=""
 outputPath=""
 sampleName=""
 
+basePath="/eos/cms/store/group/phys_diffraction/lbyl_2018"
+
 # data
 if [ $2 -eq 0 ]
 then
-  inputPath=`sed "${1}q;d" /afs/cern.ch/work/j/jniedzie/private/LightByLight2018/analysis/input_list.txt`
-  
-  if [ $3 -eq 0 ]
-  then
-    outputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/analysis/basicPlots/basicPlots_data_new"
-  elif [ $3 -eq 1 ]
-  then
-    outputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/analysis/basicPlots/basicPlots_data_new_20GeVHF"
-  fi
-  
-  
   sampleName="Data"
+#  inputPath=`sed "${1}q;d" /afs/cern.ch/work/j/jniedzie/private/LightByLight2018/analysis/input_list.txt`
+  inputPath="${basePath}/skimmed_ntuples/data_loose_selections/ntuples_loose_selections_${1}.root"
+  outputPath="${basePath}/analysis/basicPlots/basicPlots_data${suffix}"
+  
 elif [ $2 -eq 1 ]
 then
- # MC QED SC
-  inputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/mc_qed/ntuples_superchic_1034/ntuples_sc_1034/ntuples_sc_1034/191113_105005/0000/HiForestAOD_LbyL_full_sample_lbyl_reco_${1}.root"
-  
-  if [ $3 -eq 0 ]
-  then
-    outputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/analysis/basicPlots/basicPlots_mc_qed_sc_new"
-  elif [ $3 -eq 1 ]
-  then
-    outputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/analysis/basicPlots/basicPlots_mc_qed_sc_new_20GeVHF"
-  fi
-  
-  
   sampleName="QED_SC"
+  inputPath="${basePath}/mc_qed/ntuples_superchic_1034/ntuples_sc_1034/ntuples_sc_1034/191113_105005/0000/HiForestAOD_LbyL_full_sample_lbyl_reco_${1}.root"
+  outputPath="${basePath}/analysis/basicPlots/basicPlots_mc_qed_sc${suffix}"
 elif [ $2 -eq 2 ]
 then
-  # MC QED SL
-#  inputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/mc_qed/ntuples_superchic_1034/ntuples_sc_1034/ntuples_sc_1034/191113_105005/0000/HiForestAOD_LbyL_full_sample_lbyl_reco_${1}.root"
-  outputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/analysis/basicPlots/basicPlots_mc_qed_sl_new"
-  sampleName="QED_SL"
+  sampleName="CEP"
+  inputPath="${basePath}/mc_cep/ntuples_1034/ntuples_cep_1034/ntuples_cep_1034/200211_054704/0000/HiForestAOD_cep_${1}.root"
+  outputPath="${basePath}/analysis/basicPlots/basicPlots_mc_cep_sc${suffix}"
 elif [ $2 -eq 3 ]
 then
-   # MC CEP SC
-  inputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/mc_cep/ntuples_1034/ntuples_cep_1034/ntuples_cep_1034/200211_054704/0000/HiForestAOD_cep_${1}.root"
-  
-  if [ $3 -eq 0 ]
-  then
-    outputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/analysis/basicPlots/basicPlots_mc_cep_sc_new"
-  elif [ $3 -eq 1 ]
-  then
-    outputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/analysis/basicPlots/basicPlots_mc_cep_sc_new_20GeVHF"
-  fi
-  sampleName="CEP"
+  sampleName="LbL"
+  inputPath="${basePath}/mc_lbl/ntuples_1034/ntuples_lbl_1034/ntuples_lbl_1034/200207_114802/0000/HiForestAOD_LbyL_${1}.root"
+  outputPath="${basePath}/analysis/basicPlots/basicPlots_mc_lbl_sc${suffix}"
 elif [ $2 -eq 4 ]
 then
-    # MC LbL SC
-  inputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/mc_lbl/ntuples_1034/ntuples_lbl_1034/ntuples_lbl_1034/200207_114802/0000/HiForestAOD_LbyL_${1}.root"
-  
-  if [ $3 -eq 0 ]
-  then
-    outputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/analysis/basicPlots/basicPlots_mc_lbl_sc_new"
-  elif [ $3 -eq 1 ]
-  then
-    outputPath="/eos/cms/store/group/phys_diffraction/lbyl_2018/analysis/basicPlots/basicPlots_mc_lbl_sc_new_20GeVHF"
-  fi
-  sampleName="LbL"
+  sampleName="QED_SL"
+#  inputPath="${basePath}/mc_qed/ntuples_superchic_1034/ntuples_sc_1034/ntuples_sc_1034/191113_105005/0000/HiForestAOD_LbyL_full_sample_lbyl_reco_${1}.root"
+  outputPath="${basePath}/analysis/basicPlots/basicPlots_mc_qed_sl${suffix}"
 fi
 
 mkdir -p $outputPath
