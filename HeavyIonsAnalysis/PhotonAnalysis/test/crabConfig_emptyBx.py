017508ed16b0c33f4cda4660e72597e4a7245b11
Emilien Chapon <emilien.chapon@cern.ch>
2020-04-16 12:17:54 +0200

    restore deleted files, add pixTrkOnly files

--- HeavyIonsAnalysis/PhotonAnalysis/test/crabConfig_emptyBx.py
+++ HeavyIonsAnalysis/PhotonAnalysis/test/crabConfig_emptyBx.py
@@ -2,7 +2,7 @@ from CRABClient.UserUtilities import config, getUsernameFromSiteDB
 config = config()
 
 #config.section_('General')
-config.General.requestName = 'ntuples_data_lbl_tracker_branches'
+config.General.requestName = 'ntuples_emptyBx_tracker_branches_new'
 config.General.workArea = 'crab_projects'
 config.General.transferOutputs = True
 config.General.transferLogs = True
@@ -14,7 +14,7 @@ config.JobType.psetName = 'runForestAOD_pponAA_DATA_103X.py'
 config.JobType.allowUndistributedCMSSW = True
 
 
-config.Data.inputDataset ='/HIForward/HIRun2018A-ForLByL-v2/AOD'
+config.Data.inputDataset ='/HIEmptyBX/HIRun2018A-27Feb2019-v1/AOD'
 config.Data.lumiMask = "/afs/cern.ch/cms/CAF/CMSCOMM/COMM_DQM/certification/Collisions18/HI/PromptReco/Cert_326381-327564_HI_PromptReco_Collisions18_JSON.txt"
 config.Data.splitting = 'Automatic'
 
@@ -25,8 +25,8 @@ config.Data.runRange = '326381-327564'
 # config.Data.splitting = 'FileBased'
 # config.Data.unitsPerJob = 1
 
-config.Data.outLFNDirBase = '/store/group/phys_diffraction/lbyl_2018/HIForward_Reco/ntuples/ntuples_data_tracker_branches/'
+config.Data.outLFNDirBase = '/store/group/phys_diffraction/lbyl_2018/HIEmptyBX/ntuples/ntuples_tracker_branches_new/'
 config.Data.allowNonValidInputDataset = True
 config.Data.publication = False
-config.Data.outputDatasetTag = 'ntuples_data_lbl_tracker_branches'
+config.Data.outputDatasetTag = 'ntuples_emptyBx_tracker_branches_new'
 config.Site.storageSite = 'T2_CH_CERN'
--- HeavyIonsAnalysis/PhotonAnalysis/test/runForestAOD_pponAA_NOLBL_pixTrkOnly_DATA_103X.py
+++ HeavyIonsAnalysis/PhotonAnalysis/test/runForestAOD_pponAA_NOLBL_pixTrkOnly_DATA_103X.py
@@ -16,9 +16,8 @@ process = cms.Process('HiForest')
 process.load("HeavyIonsAnalysis.JetAnalysis.HiForest_cff")
 process.HiForest.inputLines = cms.vstring("HiForest 103X")
 import subprocess, os
-version = ''
-# version = subprocess.check_output(['git',
-#     '-C', os.path.expandvars('$CMSSW_BASE/src'), 'describe', '--tags'])
+version = subprocess.check_output(['git',
+    '-C', os.path.expandvars('$CMSSW_BASE/src'), 'describe', '--tags'])
 if version == '':
     version = 'no git info'
 process.HiForest.HiForestVersion = cms.string(version)
@@ -31,8 +30,16 @@ process.source = cms.Source("PoolSource",
     duplicateCheckMode = cms.untracked.string("noDuplicateCheck"),
     fileNames = cms.untracked.vstring(
         #"file:/afs/cern.ch/work/r/rbi/public/forest/HIHardProbes_HIRun2018A-PromptReco-v2_AOD.root"
-        "file:/eos/cms/store/group/phys_diffraction/lbyl_2018/B3B60266-96AA-A146-8658-4FE0F40F9D00.root"
         # 'file:B3B60266-96AA-A146-8658-4FE0F40F9D00.root'
+        '/store/hidata/HIRun2018A/HIForward/AOD/04Apr2019-v1/280000/82A92481-E129-6947-AE71-F7ED22FF47A6.root',
+        '/store/hidata/HIRun2018A/HIForward/AOD/04Apr2019-v1/280000/82AF9679-492D-DE4B-8D56-0C69ED53DF0F.root',
+        '/store/hidata/HIRun2018A/HIForward/AOD/04Apr2019-v1/280000/815D19EE-371A-074D-8146-2E9E6ACE7AA8.root',
+        '/store/hidata/HIRun2018A/HIForward/AOD/04Apr2019-v1/280000/811FB2E8-4159-F244-B079-3F5AE4EB0C9E.root',
+        '/store/hidata/HIRun2018A/HIForward/AOD/04Apr2019-v1/280000/80804F44-7F54-1A48-B3B7-9FFB80AC0ACC.root',
+        '/store/hidata/HIRun2018A/HIForward/AOD/04Apr2019-v1/280000/806F77DE-A9C3-7D46-9F54-FFAB50CC9BF4.root',
+        '/store/hidata/HIRun2018A/HIForward/AOD/04Apr2019-v1/280000/80316E5C-2AF8-2740-8A49-5EA8285C2DEB.root',
+        '/store/hidata/HIRun2018A/HIForward/AOD/04Apr2019-v1/280000/802A2D93-8662-6A41-A648-8D7165213CEC.root',
+        '/store/hidata/HIRun2018A/HIForward/AOD/04Apr2019-v1/280000/7F7D595B-1868-C14B-AE04-DC845AF0B721.root'
         #"file:/afs/cern.ch/work/r/rchudasa/private/hiforest_1034/CMSSW_10_3_4/src/from_old_1034_repo/step3_RAW2DIGI_L1Reco_RECO.root"
         #"file:/afs/cern.ch/work/r/rchudasa/private/hiforest_1034/CMSSW_10_3_4/src/from_old_1034_repo/wo_lbyl_mod/step3_RAW2DIGI_L1Reco_RECO.root"
 ),
@@ -40,7 +47,7 @@ process.source = cms.Source("PoolSource",
 
 # Number of events we want to process, -1 = all events
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(10000)
+    input = cms.untracked.int32(500000)
     )
 
 ###############################################################################
@@ -55,8 +62,8 @@ process.load('FWCore.MessageService.MessageLogger_cfi')
 
 from Configuration.AlCa.GlobalTag import GlobalTag
 #process.GlobalTag = GlobalTag(process.GlobalTag, '103X_dataRun2_Prompt_v2', '')
-#process.GlobalTag = GlobalTag(process.GlobalTag, '103X_dataRun2_Prompt_fixEcalADCToGeV_v1', '')
-process.GlobalTag = GlobalTag(process.GlobalTag, '103X_dataRun2_Prompt_LowPtPhotonReg_v1', '')
+process.GlobalTag = GlobalTag(process.GlobalTag, '103X_dataRun2_Prompt_fixEcalADCToGeV_v1', '')
+# process.GlobalTag = GlobalTag(process.GlobalTag, '103X_dataRun2_Prompt_LowPtPhotonReg_v1', '')
 process.HiForest.GlobalTagLabel = process.GlobalTag.globaltag
 
 print('\n\033[31m~*~ USING CENTRALITY TABLE FOR PbPb 2018 DATA ~*~\033[0m\n')
@@ -79,7 +86,7 @@ process.centralityBin.centralityVariable = cms.string("HFtowers")
 
 process.TFileService = cms.Service("TFileService",
     #fileName = cms.string("data_HiForestAOD_wohlt_eta2p3_norechit.root"))
-    fileName = cms.string("data_HiForestAOD.root"))
+    fileName = cms.string("data_HiForestAOD_swiss_cross.root"))
 
 ###############################################################################
 # Additional Reconstruction and Analysis: Main Body
@@ -89,7 +96,7 @@ process.TFileService = cms.Service("TFileService",
 # Jets
 #############################
 # jet reco sequence
-# process.load('HeavyIonsAnalysis.JetAnalysis.fullJetSequence_pponAA_data_cff')
+process.load('HeavyIonsAnalysis.JetAnalysis.fullJetSequence_pponAA_data_cff')
 
 process.load('HeavyIonsAnalysis.JetAnalysis.hiFJRhoAnalyzer_cff')
 process.load("HeavyIonsAnalysis.JetAnalysis.pfcandAnalyzer_cfi")
@@ -133,9 +140,26 @@ process.load('HeavyIonsAnalysis.PhotonAnalysis.correctedElectronProducer_cfi')
 process.correctedElectrons.correctionFile = SSHIRun2018A
 
 process.load('HeavyIonsAnalysis.PhotonAnalysis.ggHiNtuplizer_cfi')
-process.ggHiNtuplizer.doGenParticles = False
-#process.ggHiNtuplizerGED.doGenParticles = False
-#process.ggHiNtuplizerGED.gsfElectronLabel = "correctedElectrons"
+process.ggHiNtuplizer_pixelOnly = process.ggHiNtuplizer.clone()
+process.ggHiNtuplizer_pixelOnly.doGenParticles = cms.bool(False)
+process.ggHiNtuplizer_pixelOnly.doPixelTracks = cms.bool(True)
+process.ggHiNtuplizer_pixelOnly.doSuperClusters = cms.bool(False)
+process.ggHiNtuplizer_pixelOnly.doElectrons        = cms.bool(False)
+process.ggHiNtuplizer_pixelOnly.doPhotons          = cms.bool(False)
+process.ggHiNtuplizer_pixelOnly.doMuons            = cms.bool(False)
+process.ggHiNtuplizer_pixelOnly.runOnParticleGun   = cms.bool(False)
+process.ggHiNtuplizer_pixelOnly.useValMapIso       = cms.bool(False)
+process.ggHiNtuplizer_pixelOnly.doElectronVID      = cms.bool(False)
+process.ggHiNtuplizer_pixelOnly.doEleERegression   = cms.bool(False)
+process.ggHiNtuplizer_pixelOnly.doEffectiveAreas   = cms.bool(False)
+process.ggHiNtuplizer_pixelOnly.doPhoERegression   = cms.bool(False)
+process.ggHiNtuplizer_pixelOnly.doRecHitsEB        = cms.bool(False)
+process.ggHiNtuplizer_pixelOnly.doRecHitsEE        = cms.bool(False)
+process.ggHiNtuplizer_pixelOnly.doGeneralTracks    = cms.bool(False)
+process.ggHiNtuplizer_pixelOnly.doTrackerHits      = cms.bool(False)
+process.ggHiNtuplizer_pixelOnly.doCaloTower        = cms.bool(False)
+#process.ggHiNtuplizer_pixelOnlyGED.doGenParticles = False
+#process.ggHiNtuplizer_pixelOnlyGED.gsfElectronLabel = "correctedElectrons"
 
 ###############################################################################
 
@@ -180,13 +204,29 @@ process.rechitanalyzerpp.zdcRecHitSrc = cms.InputTag("QWzdcreco")
 #Recover peripheral primary vertices
 #https://twiki.cern.ch/twiki/bin/view/CMS/HITracking2018PbPb#Peripheral%20Vertex%20Recovery
 process.load("RecoVertex.PrimaryVertexProducer.OfflinePrimaryVerticesRecovery_cfi")
-process.load("TrackingTools.TransientTrack.TransientTrackBuilder_cfi")
 
 # clean bad PF candidates
 if cleanJets:
     process.load("RecoHI.HiJetAlgos.HiBadParticleFilter_cfi")
     process.pfBadCandAnalyzer = process.pfcandAnalyzer.clone(pfCandidateLabel = cms.InputTag("filteredParticleFlow","cleaned"))
     process.pfFilter = cms.Path(process.filteredParticleFlow + process.pfBadCandAnalyzer)
+
+### FILTERS ###
+# Trigger Filter
+from HLTrigger.HLTfilters.hltHighLevel_cfi import hltHighLevel
+process.hltFilter = hltHighLevel.clone(
+      HLTPaths =  [
+         'HLT_HIUPC_DoubleEG2_NotMBHF2AND_v1',
+         'HLT_HIUPC_DoubleEG2_NotMBHF2AND_SinglePixelTrack_v1',
+         'HLT_HIUPC_DoubleEG2_NotMBHF2AND_SinglePixelTrack_MaxPixelTrack_v1',
+         'HLT_HIUPC_DoubleEG5_NotMBHF2AND_v1',
+         'HLT_HIUPC_DoubleEG5_NotMBHF2AND_SinglePixelTrack_v1',
+         'HLT_HIUPC_DoubleEG5_NotMBHF2AND_SinglePixelTrack_MaxPixelTrack_v1'
+         ] , 
+      TriggerResultsTag = cms.InputTag("TriggerResults","","HLT") , 
+      throw = False , andOr = True , eventSetupPathsKey = cms.string( '' ) 
+      )
+process.triggerSelection = cms.Sequence( process.hltFilter )
 
 #########################
 # Main analysis list
@@ -200,26 +240,27 @@ if cleanJets:
 #process.hltfilter.TriggerResultsTag = cms.InputTag("TriggerResults","","HLT")
 
 process.ana_step = cms.Path(
+    process.triggerSelection + 
     process.offlinePrimaryVerticesRecovery +
     #process.HiForest +
-    process.hltanalysis +
+    # process.hltanalysis +
     ##process.hltobject +
-    process.l1object +
+    # process.l1object +
     #process.centralityBin +
     #process.hiEvtAnalyzer +
     #process.jetSequence +
     #process.hiPuRhoR3Analyzer + 
     #process.correctedElectrons +
-    process.ggHiNtuplizer +
+    process.ggHiNtuplizer_pixelOnly
     #process.ggHiNtuplizerGED +
     #process.hiFJRhoAnalyzer +
     #process.hiFJRhoAnalyzerFinerBins +
     #process.pfcandAnalyzer +
     #process.pfcandAnalyzerCS +
     #process.trackSequencesPP +
-    process.zdcdigi +
-    process.QWzdcreco +
-    process.rechitanalyzerpp
+    # process.zdcdigi +
+    # process.QWzdcreco +
+    # process.rechitanalyzerpp
     )
 
 # # edm output for debugging purposes
