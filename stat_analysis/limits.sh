4f2b4c86f82d9fb240b5fb202acee3a2d67c7d21
Emilien Chapon <emilien.chapon@cern.ch>
2020-04-16 14:37:02 +0200

    stuff for ALp limits (from FSQ-16-012)

---
+++ stat_analysis/limits.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+statonly="--freezeParameters all"
+
+echo "##################"
+echo "#  mass = 5 GeV  #"
+echo "##################"
+
+combine -M AsymptoticLimits datacard_ged_hinvmass_axion0.txt $statonly
+
+echo "##################"
+echo "#  mass = 6 GeV  #"
+echo "##################"
+
+combine -M AsymptoticLimits datacard_ged_hinvmass_axion1.txt $statonly
+
+echo "##################"
+echo "#  mass = 9 GeV  #"
+echo "##################"
+
+combine -M AsymptoticLimits datacard_ged_hinvmass_axion2.txt $statonly
+
+echo "###################"
+echo "#  mass = 11 GeV  #"
+echo "###################"
+
+combine -M AsymptoticLimits datacard_ged_hinvmass_axion3.txt $statonly
+
+echo "###################"
+echo "#  mass = 14 GeV  #"
+echo "###################"
+
+combine -M AsymptoticLimits datacard_ged_hinvmass_axion4.txt $statonly
+
+echo "###################"
+echo "#  mass = 16 GeV  #"
+echo "###################"
+
+combine -M AsymptoticLimits datacard_ged_hinvmass_axion5.txt $statonly
+
+echo "###################"
+echo "#  mass = 22 GeV  #"
+echo "###################"
+
+combine -M AsymptoticLimits datacard_ged_hinvmass_axion6.txt $statonly
+
+echo "###################"
+echo "#  mass = 30 GeV  #"
+echo "###################"
+
+combine -M AsymptoticLimits datacard_ged_hinvmass_axion7.txt $statonly
+
+echo "###################"
+echo "#  mass = 90 GeV  #"
+echo "###################"
+
+combine -M AsymptoticLimits datacard_ged_hinvmass_axion8.txt $statonly
+
+# echo ""
+# echo ""
+# echo ""
+# echo "################################"
+# echo "# high mass with counting only #"
+# echo "################################"
+# echo ""
+# echo ""
+# echo ""
+
+# echo "###################"
+# echo "#  mass = 30 GeV  #"
+# echo "###################"
+
+# combine -M AsymptoticLimits datacard_ged_counting_axion_5.txt
+
+# echo "###################"
+# echo "#  mass = 90 GeV  #"
+# echo "###################"
+
+# combine -M AsymptoticLimits datacard_ged_counting_axion_6.txt
