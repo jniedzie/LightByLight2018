ec6d881c3caeaa99050486c4f8c342723ee18ae1
Pranati Jana <tukupranati@gmail.com>
2022-06-23 10:38:59 +0200

    add EleMu

--- analysis/include/PhysObjectProcessor.hpp
+++ analysis/include/PhysObjectProcessor.hpp
@@ -36,6 +36,9 @@ public:
  
   /// Returns a lorentz vector which is a sum of provided objects, assuming they are muons
   TLorentzVector GetDimuon(const PhysObject &a, const PhysObject &b);
+  
+  ///Returns a lorentz vector which is a sum of provided objects,assuming they are electron+muon
+  TLorentzVector GetEleMu(const PhysObject &a, const PhysObject &b);
 
  
   /// Returns acoplanarity of pair of objects
